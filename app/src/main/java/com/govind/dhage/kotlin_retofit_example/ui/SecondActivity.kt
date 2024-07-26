1fd04be1923c2e8e4a5f2719ebaf20b21f03312c
Govind Dhage <govinddhage97@gmail.com>
2024-07-26 12:38:56 +0530

    second activity added

---
+++ app/src/main/java/com/govind/dhage/kotlin_retofit_example/ui/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.govind.dhage.kotlin_retofit_example.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.govind.dhage.kotlin_retofit_example.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
